02d5e359ec351d210b60c35ba5615092ee2f0e80
Syed Faraz <119295880+Syed-Farazuddin@users.noreply.github.com>
2024-08-30 11:09:19 +0530

    Delete lib/core/firebase_options.dart
    keys

--- lib/core/firebase_options.dart
+++
@@ -1,8 +0,0 @@
-class Options {
-  static const String apiKey = "AIzaSyAUmyA6e_E-TYpavP40qE7N4wVkQvvasiU";
-  static const String authDomain = "my-insta-clone-a5f2d.firebaseapp.com";
-  static const String projectId = "my-insta-clone-a5f2d";
-  static const String storageBucket = "my-insta-clone-a5f2d.appspot.com";
-  static const String messagingSenderId = "753753298387";
-  static const String appId = "1:753753298387:web:4191abfc3d8b5a1d89cb26";
-}
